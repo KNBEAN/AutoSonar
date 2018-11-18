858b02f3fa81fc9875fb020395d429f98291e7f7
cKaliban <228812@student.pwr.edu.pl>
2018-11-18 20:26:00 +0100

    Kod

--- code/src/main.c
+++ code/src/main.c
@@ -2,7 +2,7 @@
 #include <util/delay.h>
 #include <usound.h>
 #include <uart.h>
-// #include <
+
 
 #ifndef F_CPU  
 #define F_CPU 1000000UL  
@@ -10,16 +10,19 @@
 
 int main(void)
 {
+    //Servo steering init
     DDRB = 0xFF;
     ICR1=20000;
 	TCCR1A  = (0<<COM1A0)|(1<<COM1A1)|(0<<COM1B0)|(0<<COM1B1)|(0<<FOC1A)|(0<<FOC1B)|(1<<WGM11)|(0<<WGM10);
 	TCCR1B =  (0<<ICNC1)|(0<<ICES1)|(1<<WGM13)|(1<<WGM12)|(0<<CS12)|(0<<CS11)|(1<<CS10);
+    //
 
     uint32_t distance;
 
     usound_init();
     UART_Init(4800);
     _delay_ms(500);
+    
     while (1)
     {
         for(OCR1A = 800; OCR1A != 2200; OCR1A+=10){
